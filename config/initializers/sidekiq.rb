91b69a2824e8dc28a145a61cd69f90d4367a0a93
Abubakar Diallo <dialloabubakar5861@gmail.com>
2025-04-14 19:50:56 +0100

    feat: setup redis for prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" } }
+  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://red-cvul80q4d50c73b2n850:6379" } }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" } }
+  config.redis = { url: ENV.fetch("REDIS_URL") { "redis://red-cvul80q4d50c73b2n850:6379" } }
 end
